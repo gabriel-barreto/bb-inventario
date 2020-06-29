9502fc6296754593e4df230dc2f8e9fff3c2fe8e
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-06-29 02:27:58 -0300

    feat(config): add config to reactotron

---
+++ src/config/reactotron.config.ts
@@ -0,0 +1,21 @@
+import Reactotron from 'reactotron-react-native';
+
+import App from '../../app.json';
+
+declare global {
+  interface Console {
+    tron: any;
+  }
+}
+
+if (__DEV__) {
+  const tron = Reactotron.configure({
+    name: App.expo.name,
+    host: '192.168.56.212',
+  })
+    .useReactNative()
+    .connect();
+
+  console.tron = tron;
+  console.tron.clear();
+}
