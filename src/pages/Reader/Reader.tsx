7a0a2456eb01f51776855dbddf4592fbced05aa4
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-07-02 02:24:35 -0300

    feat(reader-page): implement barcode scanner

--- src/pages/Reader/Reader.tsx
+++ src/pages/Reader/Reader.tsx
@@ -1,6 +1,79 @@
-import React from 'react';
-import { View } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import * as S from './styled';
+
+type ScannedCode = {
+  type: string;
+  data: string;
+};
 
 export default function ReaderPage() {
-  return <View />;
+  const [hasPermission, setPermission] = useState<Boolean | null>(null);
+  const [scanned, setScanned] = useState<Boolean>(false);
+  const navigator = useNavigation();
+
+  useEffect(() => {
+    BarCodeScanner.requestPermissionsAsync().then(({ granted }) => {
+      setPermission(granted);
+    });
+  }, []);
+
+  function onCodeScanned({ type, data }: ScannedCode) {
+    if (scanned) return;
+
+    setScanned(true);
+    console.tron.log({ type, data });
+  }
+
+  function onReadAgainPress() {
+    setScanned(false);
+  }
+
+  navigator.setOptions({ title: 'Registrar Item' });
+
+  if (hasPermission === null)
+    return (
+      <S.Container>
+        <S.Message>Solicitando permissão para utilizar a câmera</S.Message>
+      </S.Container>
+    );
+
+  if (hasPermission === false)
+    return (
+      <S.Container>
+        <S.Warning>Sem permissão para utilizar a câmera</S.Warning>
+      </S.Container>
+    );
+
+  return (
+    <S.Container>
+      <S.Scanner
+        style={StyleSheet.absoluteFillObject}
+        onBarCodeScanned={onCodeScanned}
+        needsOffscreenAlphaCompositing
+      />
+
+      {!scanned ? (
+        <S.Indicator source={require('../../assets/reader-indicator.png')} />
+      ) : null}
+
+      {scanned && <S.ActionsOverlay />}
+
+      <S.Actions>
+        {scanned && (
+          <>
+            <S.Reset onPress={onReadAgainPress}>
+              <S.ResetLabel>Ler novamente!</S.ResetLabel>
+            </S.Reset>
+            <S.Next onPress={() => {}}>
+              <S.NextLabel>Continuar</S.NextLabel>
+            </S.Next>
+          </>
+        )}
+      </S.Actions>
+    </S.Container>
+  );
 }
---
+++ src/pages/Reader/styled.ts
@@ -0,0 +1,79 @@
+import { RectButton } from 'react-native-gesture-handler';
+import styled from 'styled-components/native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+// ==> Base
+const ActionLabel = styled.Text`
+  font-size: ${({ theme }) => theme.sizes.normal};
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+const Action = styled(RectButton)`
+  align-items: center;
+  height: 48px;
+  justify-content: center;
+  width: 100%;
+`;
+// End: Base
+
+export const Container = styled.View`
+  align-items: center;
+  background-color: #000;
+  flex: 1;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const Scanner = styled(BarCodeScanner)`
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Message = styled.Text`
+  color: ${({ theme }) => theme.colors.secondary.hex()};
+  font-size: ${({ theme }) => theme.sizes.strong};
+  text-align: center;
+`;
+
+export const Warning = styled(Message)`
+  color: ${({ theme }) => theme.colors.error.hex()};
+`;
+
+export const Actions = styled.View`
+  align-items: center;
+  bottom: 32px;
+  justify-content: center;
+  padding: 0 32px;
+  position: absolute;
+  width: 100%;
+`;
+
+export const Reset = styled(Action)`
+  background-color: ${({ theme }) => theme.colors.lightest.hex()};
+`;
+
+export const ResetLabel = styled(ActionLabel)`
+  color: ${({ theme }) => theme.colors.secondary.hex()};
+`;
+
+export const NextLabel = styled(ActionLabel)`
+  color: ${({ theme }) => theme.colors.lightest.hex()};
+`;
+
+export const Next = styled(Action)`
+  background-color: ${({ theme }) => theme.colors.secondary.hex()};
+  margin: 10px 0 0 0;
+`;
+
+export const ActionsOverlay = styled.View`
+  background-color: #000;
+  height: 100%;
+  width: 100%;
+`;
+
+export const Indicator = styled.Image`
+  height: 72%;
+  resize-mode: contain;
+  width: 72%;
+`;
