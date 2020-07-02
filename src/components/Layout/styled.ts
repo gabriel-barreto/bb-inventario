16a78e6cc0b028cc254fb0b79a3539660b8778fa
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-07-02 00:50:34 -0300

    refactor(layout): replace ScrollView with View

--- src/components/Layout/styled.ts
+++ src/components/Layout/styled.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components/native';
 
-export const Layout = styled.ScrollView`
+export const Layout = styled.View`
   flex: 1;
   padding: 8px 0 0 0;
 `;
 
-export const ContainerLayout = styled.ScrollView`
+export const ContainerLayout = styled.View`
   flex: 1;
   padding: 32px;
 `;
