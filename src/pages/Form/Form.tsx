39f02672f5dbeff4e9f42deb75c684a3740c5c79
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-07-03 22:22:15 -0300

    feat(form-page): render new iem form

--- src/pages/Form/Form.tsx
+++ src/pages/Form/Form.tsx
@@ -1,6 +1,52 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+
+import { Input, Layout } from '../../components';
+import fields from './fields';
+
+type Route = {
+  params: {
+    code: string;
+  };
+};
 
 export default function FormPage() {
-  return <View />;
+  const navigator = useNavigation();
+  const { params } = useRoute() as Route;
+  const [payload, setPayload] = useState<Record<string, string>>({
+    code: '',
+    name: '',
+    sector: '',
+    userName: '',
+    userRegistry: '',
+  });
+
+  function onInput(name: string, value: string) {
+    setPayload((prev) => ({ ...prev, [name]: value }));
+    console.tron.log(payload);
+  }
+
+  useEffect(() => {
+    setPayload((prev) => ({ ...prev, code: params.code }));
+  }, [params.code]);
+
+  navigator.setOptions({ title: 'Registrar Item' });
+  return (
+    <Layout container>
+      <ScrollView>
+        {fields.map(({ label, name, placeholder, ...rest }) => (
+          <Input
+            blurOnSubmit
+            key={name}
+            label={label}
+            placeholder={placeholder}
+            value={payload[name]}
+            onChange={(value) => onInput(name, value)}
+            {...rest}
+          />
+        ))}
+      </ScrollView>
+    </Layout>
+  );
 }
---
+++ src/pages/Form/fields.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    label: 'Identificação do Item*:',
+    name: 'name',
+    placeholder: 'Exemplo: Leitor Biométrico',
+    autoFocus: true,
+  },
+  { label: 'Setor*:', name: 'sector', placeholder: 'Exemplo: Caixas' },
+  {
+    label: 'Funcionário*:',
+    name: 'userName',
+    placeholder: 'Exemplo: Leandro A. Barreto',
+    autocompleteType: 'name',
+    autoCorrect: true,
+  },
+  {
+    label: 'Matrícula Funcionário*:',
+    name: 'userRegistry',
+    placeholder: 'Exemplo: 123456-7',
+    keyboardType: 'number-pad',
+  },
+];
