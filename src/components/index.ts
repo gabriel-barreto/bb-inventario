b4ad156f158f75f17f40acd1baf0386089d6d730
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-07-04 22:00:25 -0300

    feat(components): export loader and warning

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Input } from './Input';
 export { default as Layout } from './Layout';
+export { default as Loader } from './Loader';
+export { default as Warning } from './Warning';
