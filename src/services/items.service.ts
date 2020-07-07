3cb58161fd4e4698f25fd31a928b8a3be795141c
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-07-07 00:15:41 -0300

    feat(items-service): implements all methods

--- src/services/items.service.ts
+++ src/services/items.service.ts
@@ -27,26 +27,71 @@ function list(): Promise<ListItem[]> {
       .firestore()
       .collection(COLLECTION)
       .onSnapshot((snapshot) => {
+        if (snapshot.empty) return resolve([]);
+
         snapshot.forEach((doc) => {
           const { id } = doc;
           const { code, name, sector } = doc.data();
           docs.push({ code, id, name, sector });
         });
-        resolve(docs);
+        return resolve(docs);
       });
   });
 }
 
-async function fetchOne(itemId: string): Promise<Item> {
-  return new Promise((resolve, _) => {
+function fetchOne(itemId: string): Promise<Item | null> {
+  return new Promise((resolve, reject) => {
     firebase
       .firestore()
       .collection(COLLECTION)
       .doc(itemId)
-      .onSnapshot((snapshot) =>
-        resolve({ ...snapshot.data(), id: snapshot.id } as Item),
-      );
+      .get()
+      .then((doc) => {
+        if (!doc.exists) return resolve(null);
+        return resolve({ ...doc.data(), id: doc.id } as Item);
+      })
+      .catch(reject);
   });
 }
 
-export default { list, fetchOne };
+function create(payload: Partial<Item>) {
+  try {
+    const ref = firebase.firestore().collection(COLLECTION).doc();
+    return new Promise((resolve, _) => {
+      const created = ref.set(payload);
+      resolve({ ...created, id: ref.id });
+    });
+  } catch (ex) {
+    return Promise.reject(ex);
+  }
+}
+
+async function remove(itemId: string) {
+  try {
+    const ref = firebase.firestore().collection(COLLECTION).doc(itemId);
+    return ref.get().then(async (doc) => {
+      if (!doc.exists) return false;
+
+      await ref.delete();
+      return true;
+    });
+  } catch (ex) {
+    return Promise.reject(ex);
+  }
+}
+
+async function update(itemId: string, payload: Partial<Item>) {
+  try {
+    const ref = firebase.firestore().collection(COLLECTION).doc(itemId);
+    return ref.get().then(async (doc) => {
+      if (!doc.exists) return null;
+
+      await ref.set({ ...doc.data, ...payload });
+      return { ...doc.data, ...payload };
+    });
+  } catch (ex) {
+    return Promise.reject(ex);
+  }
+}
+
+export default { list, fetchOne, create, remove, update };
