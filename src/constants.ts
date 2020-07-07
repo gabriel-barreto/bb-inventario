574cd99ec2072b56686afacf617f2aee5fcccb1d
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-07-07 00:55:07 -0300

    feat(constants): add failToRemove warning message

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
 export const failToCreateWarning =
   'Ocorreu uma falha ao tentar registrar o item. Verifique os dados, aguarde alguns instantes e tente novamente!';
+
+export const failToRemoveWarning =
+  'Ocorreu um erro ao tentar remove o item. Por favor, aguarde alguns instantes e tente novamente!';
