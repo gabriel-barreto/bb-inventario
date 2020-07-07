29e3e6666529866712ce35b432f275d1641495d2
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-07-07 00:55:46 -0300

    feat(details): implement delete button behavior

--- src/pages/Details/DeleteButton/DeleteButton.tsx
+++ src/pages/Details/DeleteButton/DeleteButton.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import { useRoute } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { ActivityIndicator, Alert } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { MaterialIcons } from '@expo/vector-icons';
+
+import { failToRemoveWarning } from '../../../constants';
+import { $items } from '../../../services';
 
 import * as S from './styled';
 
@@ -11,17 +15,51 @@ type Route = {
 };
 
 export default function DeleteButton() {
+  const navigator = useNavigation();
   const { params } = useRoute() as Route;
+  const [loading, setLoading] = useState(false);
+
+  function removeItem() {
+    setLoading(true);
+    $items
+      .remove(params.itemId)
+      .then(() => {
+        navigator.navigate('List', { message: '' });
+      })
+      .catch(() => {
+        Alert.alert('Oops...', failToRemoveWarning);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
 
   function onPress() {
-    console.tron.log('Remove Item', params.itemId);
+    Alert.alert(
+      'Atençao!',
+      'Tem certeza que deseja continuar? Os dados afetados não poderão ser recuperados.',
+      [
+        { text: 'Continuar', onPress: removeItem },
+        { text: 'Cancelar', style: 'cancel' },
+      ],
+    );
+  }
+
+  if (loading) {
+    return (
+      <S.Container>
+        <ActivityIndicator color="#fff" size="small" />
+      </S.Container>
+    );
   }
 
   return (
-    <S.DeleteButton onPress={onPress}>
-      <S.Icon>
-        <MaterialIcons name="delete" size={24} />
-      </S.Icon>
-    </S.DeleteButton>
+    <S.Container>
+      <S.DeleteButton onPress={onPress}>
+        <S.Icon>
+          <MaterialIcons name="delete" size={24} />
+        </S.Icon>
+      </S.DeleteButton>
+    </S.Container>
   );
 }
--- src/pages/Details/DeleteButton/styled.ts
+++ src/pages/Details/DeleteButton/styled.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components/native';
 
+export const Container = styled.View`
+  align-items: center;
+  margin: 0 16px 0 0;
+  justify-content: flex-end;
+`;
+
 export const DeleteButton = styled.TouchableOpacity`
   align-items: center;
   flex: 1;
   justify-content: center;
-  margin: 0 16px 0 0;
 `;
 
 export const Icon = styled.Text`
