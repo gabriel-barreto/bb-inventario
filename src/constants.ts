df95fac303949eaf0cd053da0b474cee8a407d14
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-07-09 09:42:31 -0300

    chore(constants): set all messages

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,17 @@
 export const failToCreateWarning =
   'Ocorreu uma falha ao tentar registrar o item. Verifique os dados, aguarde alguns instantes e tente novamente!';
 
+export const successCreatedWarning =
+  'O item foi registrado no inventário e já está disponível para consulta!';
+
+export const removeConfirmation =
+  'Tem certeza que deseja continuar? Os dados afetados não poderão ser recuperados.';
+
+export const successRemovedWarning =
+  'O item foi removido do inventário e já não está mais disponível para consulta!';
+
 export const failToRemoveWarning =
   'Ocorreu um erro ao tentar remove o item. Por favor, aguarde alguns instantes e tente novamente!';
+
+export const codeAlreadyTakenWarning =
+  'O código escaneado já se encontrado registrado em nosso inventário!';
